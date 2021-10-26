ede7f5e4b587f80d33a181b223a07d38a25b7d3a
Jasper Güldenstein <5guelden@informatik.uni-hamburg.de>
2021-10-26 14:59:08 +0200

    missing file from last commit

---
+++ controllers/referee/time.py
@@ -0,0 +1,13 @@
+class SimTime:
+    def __init__(self):
+        pass
+
+
+def format_time(s):
+    seconds = str(s % 60)
+    minutes = str(int(s / 60))
+    if len(minutes) == 1:
+        minutes = '0' + minutes
+    if len(seconds) == 1:
+        seconds = '0' + seconds
+    return minutes + ':' + seconds
