260c061f621bbe842c29b610c76eef9219b19251
Timon Engelke <timon.engelke@online.de>
2021-10-27 17:22:46 +0200

    move referee code into a single class

--- controllers/referee/blackboard.py
+++ controllers/referee/blackboard.py
@@ -4,10 +4,8 @@ import yaml
 
 
 class Blackboard:
-    def __init__(self, supervisor, game, time):
+    def __init__(self, supervisor, game, time, config):
         self.supervisor = supervisor
         self.game = game
         self.time = time
-        with open('referee_config.yaml') as f:
-            config = yaml.load(f)
-        self.config = SimpleNamespace(**config)
+        self.config = config
